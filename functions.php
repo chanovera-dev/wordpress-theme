a7d61d1be1b893faaf47453f129579eeeee0d63b
Chano Vera <contacto@chanovera.com>
2023-06-10 19:56:43 -0600

    se agregó el soporte de  breadcrumbs para el sitio

--- functions.php
+++ functions.php
@@ -30,3 +30,26 @@ register_nav_menus(
         'social' => __( 'Social', 'renata' ), 
     ) 
 );
+
+/* activa las migas de pan (breadcrumb) */
+function get_breadcrumb() {
+    echo 
+    '<a href="'.home_url().'" rel="nofollow">
+        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+        </svg>';
+        echo __('Inicio', 'renata');
+    echo '</a>';
+    if (is_category() || is_single()) {
+        echo "";
+        
+            if (is_single()) {
+                echo "";
+                
+            }
+    } elseif (is_page()) {
+        echo "";
+    } elseif (is_search()) {
+        echo "";
+    }
+}
