b59c58636cc2cd4abb46b05f8f49fc4e20f5e1fb
Chano Vera <contacto@chanovera.com>
2023-06-10 22:15:30 -0600

    se corrigió un texto en parts/contact/message.php

--- parts/contact/message.php
+++ parts/contact/message.php
@@ -1,3 +1,3 @@
 <div class="contact-message">
-    <p><?= __('Trabajemos en crear tu mejor sitio. ', 'renata') ?>, <span><?= __('Empieza hoy, llámame.', 'renata') ?></span></p>
+    <p><?= __('Trabajemos en crear tu mejor sitio ', 'renata') ?>, <span><?= __('Empieza hoy, llámame.', 'renata') ?></span></p>
 </div>
