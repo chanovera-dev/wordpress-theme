62fd5485ff68e6e8d83706aea71afa4c0e08f8cb
Chano Vera <contacto@chanovera.com>
2023-06-10 22:47:14 -0600

    se agregó home.php y asociados

---
+++ parts/sections/title-blog.php
@@ -0,0 +1,7 @@
+<section class="container title-section-wrapper">
+    <div class="section title-section">
+        <?php include (TEMPLATEPATH. '/parts/widgets/languages.php'); ?>   
+        <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
+        <h1 class="title"><?= __('Blog en español', 'renata'); ?></h1>
+    </div>
+</section>
