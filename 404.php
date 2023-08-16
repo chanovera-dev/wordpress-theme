e8a1b1bfa2c5034359071af760cc7a0e102b827d
Chano Vera <contacto@chanovera.com>
2023-08-16 07:57:18 -0600

    editando la organización del código en 404.php

--- 404.php
+++ 404.php
@@ -1,13 +1,11 @@
-<?php get_header(); ?>
-<main id="main">
-    <?php include(TEMPLATEPATH . '/parts/sections/title-contact.php'); ?>
-    <section class="container">
-        <div class="section content-section">
-            <?php include(TEMPLATEPATH . '/parts/404/message.php'); ?>
-            <?php include(TEMPLATEPATH . '/parts/404/contact-form.php'); ?>
-            <?php include(TEMPLATEPATH . '/parts/widgets/address.php'); ?>
-
-        </div>
-    </section>  
-</main>
-<?php get_footer(); ?>
+<?php
+    get_header();
+    echo '<main id="main">';
+    include(TEMPLATEPATH . '/parts/sections/title-contact.php');
+    echo '<section class="container"><div class="section content-section">';
+    include(TEMPLATEPATH . '/parts/404/message.php');
+    include(TEMPLATEPATH . '/parts/404/contact-form.php');
+    include(TEMPLATEPATH . '/parts/widgets/address.php');
+    echo '</div></section></main>';
+    get_footer();
+?>
