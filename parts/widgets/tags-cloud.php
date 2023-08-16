5bab1a9b7fa71ea6e5104c1f9cebe5dc00790fcc
Chano Vera <contacto@chanovera.com>
2023-08-16 06:55:07 -0600

    condicional al widget de etiquetas para que no muestre nada si no hay etiquetas

--- parts/widgets/tags-cloud.php
+++ parts/widgets/tags-cloud.php
@@ -1,4 +1,4 @@
-<?php if ( function_exists( 'wp_tag_cloud' ) ) : ?>
+<?php if ( function_exists( 'wp_tag_cloud' ) && wp_count_terms( 'post_tag' ) > 0 ) : ?>
     <div class="tags-widget">
         <h3 class="widget-title"><?php echo __('Nube de etiquetas', 'renata'); ?></h3>
         <?php wp_tag_cloud( array(
