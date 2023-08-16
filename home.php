436c62f0cab7ab41ef564916bb4f783d1c36d0e9
Chano Vera <contacto@chanovera.com>
2023-08-16 07:32:36 -0600

    condicional para el aside del blog

--- home.php
+++ home.php
@@ -10,6 +10,7 @@
 
             <div class="articles">
                 <?php
+                
                     if ( have_posts() ){
                         
                         while( have_posts() ){
@@ -27,7 +28,17 @@
                 ?>
             </div>
             
-            <?php include(TEMPLATEPATH . '/parts/sidebars/blog.php'); ?>
+            <?php 
+            
+                $post_count = wp_count_posts();
+
+                if ( $post_count->publish > 0 ) :
+
+                    include(TEMPLATEPATH . '/parts/sidebars/blog.php');
+
+                endif;
+
+            ?>
 
         </div>
             
