7f8c4e657bb0f54ca31a14f2df2b71ea9341133b
Chano Vera <contacto@chanovera.com>
2023-08-16 08:05:06 -0600

    editando la organización del código en contact.php

--- parts/contact/message.php
+++ parts/contact/message.php
@@ -1,3 +1,3 @@
 <div class="contact-message">
-    <p><?= __('Trabajemos en crear tu mejor sitio ', 'renata') ?>, <span><?= __('empieza hoy, llámame.', 'renata') ?></span></p>
+    <p><?= __('Trabajemos en crear tu mejor sitio', 'renata') ?>, <span><?= __('empieza hoy, llámame.', 'renata') ?></span></p>
 </div>
