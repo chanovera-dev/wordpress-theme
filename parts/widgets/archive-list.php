8e1bb51bdc5a0a313305a7a1584fca48ba83593c
Chano Vera <contacto@chanovera.com>
2023-08-16 07:18:26 -0600

    condicional al widget de archivos

--- parts/widgets/archive-list.php
+++ parts/widgets/archive-list.php
@@ -1,4 +1,9 @@
+<?php
+$post_count = wp_count_posts();
+if ( $post_count->publish > 0 ) :
+?>
 <ul class="archive-list-widget">
     <h3 class="widget-title"><?= __('Archivos', 'renata'); ?></h3>
     <?php wp_get_archives(); ?>
 </ul>
+<?php endif; ?>
