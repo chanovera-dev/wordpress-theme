5b86ffd6e1ba9ce4a9a7633aeac2610e0ed793ce
Chano Vera <contacto@chanovera.com>
2023-08-19 11:50:21 -0600

    ajuste al título del blog

--- parts/sections/title-blog.php
+++ parts/sections/title-blog.php
@@ -1,7 +1,7 @@
 <section class="container title-section-wrapper">
     <div class="section title-section">
         <?php include (TEMPLATEPATH. '/parts/widgets/languages.php'); ?>   
-        <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-        <h1 class="title"><?= __('Blog en español', 'renata'); ?></h1>
+        <div class="breadcrumb"><p><?php get_breadcrumb() . __('Blog en español', 'renata'); ?></p></div>
+        
     </div>
 </section>
