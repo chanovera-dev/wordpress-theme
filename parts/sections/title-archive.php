75669e7ba42666d6e8e0338499f779e08797ade4
Chano Vera <contacto@chanovera.com>
2023-08-19 12:06:42 -0600

    ajuste al título del blog

--- parts/sections/title-archive.php
+++ parts/sections/title-archive.php
@@ -1,7 +1,6 @@
 <section class="container title-section-wrapper">
     <div class="section title-section">
-        <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-        <?php the_archive_title( '<h1 class="title">', '</h1>' ); ?>
+        <div class="breadcrumb"><?php get_breadcrumb(); ?><p>/</p><?php the_archive_title( '<h1 class="title">', '</h1>' ); ?></div>
         <?php 
             include(TEMPLATEPATH . '/parts/widgets/languages.php'); 
         ?>
--- parts/sections/title-search.php
+++ parts/sections/title-search.php
@@ -1,7 +1,6 @@
 <section class="container title-section-wrapper">
     <div class="section title-section">
-        <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-        <h1 class="title"><?php echo __('Resultados de búsqueda para: ', 'renata'); echo the_search_query(); ?></h1>
+        <div class="breadcrumb"><?php get_breadcrumb(); ?><p>/</p><h1 class="title"><?php echo __('Resultados de búsqueda para: ', 'renata'); echo the_search_query(); ?></h1></div>
         <?php 
             include(TEMPLATEPATH . '/parts/widgets/languages.php'); 
         ?>
