895d1f11a5ea5512c687a91929539e8ad17084f2
Chano Vera <contacto@chanovera.com>
2023-08-20 00:13:28 -0600

    primeros ajustes css a single-product.css

--- templates/content-product.php
+++ templates/content-product.php
@@ -1,26 +1,28 @@
-<section class="container">
-    <div class="section">
-        <?php do_action( 'woocommerce_before_single_product' ); ?>
-    </div>
-</section>
-<section class="container">
-    <div class="section data-product">
-        <div class="product-galery">
-            <?php do_action( 'woocommerce_before_single_product_summary' ); ?>
+<div class="main-content">
+    <section class="container">
+        <div class="section">
+            <?php do_action( 'woocommerce_before_single_product' ); ?>
         </div>
-        <div class="product-summary">
-            <?php 
-                do_action( 'woocommerce_single_product_summary' );
-                do_action( 'woocommerce_simple_add_to_cart' );
-                do_action( 'woocommerce_after_add_to_cart_quantity' );
+    </section>
+    <section class="container">
+        <div class="section data-product">
+            <div class="product-galery">
+                <?php do_action( 'woocommerce_before_single_product_summary' ); ?>
+            </div>
+            <div class="product-summary">
+                <?php 
+                    do_action( 'woocommerce_single_product_summary' );
+                    do_action( 'woocommerce_simple_add_to_cart' );
+                    do_action( 'woocommerce_after_add_to_cart_quantity' );
+                ?>
+            </div>
+        </div>
+    </section>
+    <section class="container">
+        <div class="section">
+            <?php do_action( 'woocommerce_after_single_product_summary' );
+                do_action( 'woocommerce_after_main_content' );
             ?>
         </div>
-    </div>
-</section>
-<section class="container">
-    <div class="section">
-        <?php do_action( 'woocommerce_after_single_product_summary' );
-            do_action( 'woocommerce_after_main_content' );
-        ?>
-    </div>
-</section>
+    </section>
+</div>
