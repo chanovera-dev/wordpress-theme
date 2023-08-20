314c5ee30787c29e9e69a3fde9c44f89f771141f
Chano Vera <contacto@chanovera.com>
2023-08-19 18:18:41 -0600

    ajusta el breadcrumb de contact.php

--- parts/sections/title-contact.php
+++ parts/sections/title-contact.php
@@ -1,7 +1,7 @@
 <section class="container title-section-wrapper">
     <div class="section title-section">
-        <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-        <?php the_title('<h1 class="title">', '</h1>'); ?>
+        <div class="breadcrumb"><?php get_breadcrumb(); ?><p>/</p><?php the_title('<h1 class="title">', '</h1>'); ?></div>
+        
         <?php 
             include(TEMPLATEPATH . '/parts/widgets/languages.php'); 
         ?>
