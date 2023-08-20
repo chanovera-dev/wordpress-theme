0358ff013357184b34a002f8af0af66a989bbe1d
Chano Vera <contacto@chanovera.com>
2023-08-19 23:00:25 -0600

    ajustes a los títulos de los widgets

--- parts/widgets/categories.php
+++ parts/widgets/categories.php
@@ -4,6 +4,24 @@ $post_count = wp_count_posts();
 
 if ( $post_count->publish > 0 ) :
 
-wp_list_categories();
+echo '<div class="categories">';
+
+    echo '<ul>';
+
+    $args = array(
+        'orderby' => 'name',
+        'order' => 'ASC',
+        'style' => 'list',
+        'show_count' => 1, // Mostrar el número de entradas en cada categoría
+        'title_li' => '', // Dejar en blanco para evitar mostrar un título
+    );
+
+    $categories = get_categories($args);
+
+    foreach ($categories as $category) {
+        echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
+    }
+
+echo '</ul></div>';
 
 endif;
