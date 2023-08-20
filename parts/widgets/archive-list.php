9ab94b4acb83ac4da75839b43a63609b22cdfe03
Chano Vera <contacto@chanovera.com>
2023-08-19 22:31:22 -0600

    ajustes a los títulos de los widgets

--- parts/widgets/archive-list.php
+++ parts/widgets/archive-list.php
@@ -4,7 +4,7 @@ $post_count = wp_count_posts();
 
 if ( $post_count->publish > 0 ) :
 
-echo '<ul class="archive-list-widget"><h3 class="widget-title">' . __('Archivos', 'renata') . '</h3>';
+echo '<ul class="archive-list-widget"><div class="title-wrapper"><h3 class="widget-title">' . __('Archivos', 'renata') . '</h3></div>';
 wp_get_archives();
 echo '</ul>';
 endif;
