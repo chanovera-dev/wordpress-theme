e5f5a8de16662c6c4fd6c252f7f98ccee29ad8c4
Chano Vera <contacto@chanovera.com>
2023-08-20 00:05:42 -0600

    se agregaron secciones al artículo de woocommerce

--- templates/content-product.php
+++ templates/content-product.php
@@ -1,3 +1,8 @@
+<section class="container">
+    <div class="section">
+        <?php do_action( 'woocommerce_before_single_product' ); ?>
+    </div>
+</section>
 <section class="container main-content">
     <div class="section data-product">
         <div class="product-galery">
@@ -6,7 +11,16 @@
         <div class="product-summary">
             <?php 
                 do_action( 'woocommerce_single_product_summary' );
+                do_action( 'woocommerce_simple_add_to_cart' );
+                do_action( 'woocommerce_after_add_to_cart_quantity' );
             ?>
         </div>
+    </div>
+</section>
+<section class="container">
+    <div class="section">
+        <?php do_action( 'woocommerce_after_single_product_summary' );
+            do_action( 'woocommerce_after_main_content' );
+        ?>
     </div>
 </section>
