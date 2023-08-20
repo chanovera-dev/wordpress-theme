950c5fc120206ad9ee7510eaaaceeed148166d0e
Chano Vera <contacto@chanovera.com>
2023-08-20 13:11:34 -0600

    ajuste de css para 404.php

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
     get_header();
     echo '<main id="main">';
     include(TEMPLATEPATH . '/parts/sections/title-contact.php');
-    echo '<section class="container"><div class="section content-section">';
+    echo '<section class="container main-content"><div class="section content-section">';
     include(TEMPLATEPATH . '/parts/404/message.php');
     include(TEMPLATEPATH . '/parts/404/contact-form.php');
     include(TEMPLATEPATH . '/parts/widgets/address.php');
