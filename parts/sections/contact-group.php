e18c5060462f6da83be182fb8195ea5275dc0427
Chano Vera <contacto@chanovera.com>
2023-08-20 13:09:00 -0600

    ajuste de css para contact.php

--- parts/sections/contact-group.php
+++ parts/sections/contact-group.php
@@ -1,5 +1,5 @@
 <?php
-echo '<section id="contact-group" class="container"><div class="section content-section">'; 
+echo '<section id="contact-group" class="container main-content"><div class="section content-section">'; 
 include(TEMPLATEPATH . '/parts/contact/message.php');
 include(TEMPLATEPATH . '/parts/contact/contact-form.php');
 include(TEMPLATEPATH . '/parts/widgets/address.php');
