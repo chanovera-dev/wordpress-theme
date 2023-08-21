6d190a84e4484b425ab84de5c6d03fdb305fa1ba
Chano Vera <contacto@chanovera.com>
2023-08-21 17:47:38 -0600

    se crea el archivo resume.php

--- resume.php
+++ resume.php
@@ -1,8 +1,6 @@
 <?php /* Template name: Resume */
     get_header();
 
-    include(TEMPLATEPATH . '/parts/sections/title-page.php');
-    
     get_template_part( 'templates/content', 'resume' );
 
     get_footer();
--- templates/content-resume.php
+++ templates/content-resume.php
@@ -1,4 +1,5 @@
 <main id="main" class="main-content">
+    <?php include(TEMPLATEPATH . '/parts/sections/title-page.php'); ?>
     <section class="container">
         <div class="section">
             <h1>Developer</h1>
