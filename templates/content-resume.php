0643b52b41cb98b4d52cab1f05c290af01959cdd
Chano Vera <contacto@chanovera.com>
2023-08-21 22:33:44 -0600

    se agrega contenido svg a tech Skills

--- templates/content-resume.php
+++ templates/content-resume.php
@@ -117,8 +117,16 @@
             <ul class="tech">
                 <li>
                     <ul>
-                        <li>Windows</li>
-                        <li>Red Hat</li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-microsoft" viewBox="0 0 16 16">
+                                <path d="M7.462 0H0v7.19h7.462V0zM16 0H8.538v7.19H16V0zM7.462 8.211H0V16h7.462V8.211zm8.538 0H8.538V16H16V8.211z"/>
+                            </svg>
+                            Windows
+                        </li>
+                        <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="17" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M341.52 285.56c33.65 0 82.34-6.94 82.34-47 .22-6.74.86-1.82-20.88-96.24-4.62-19.15-8.68-27.84-42.31-44.65-26.09-13.34-82.92-35.37-99.73-35.37-15.66 0-20.2 20.17-38.87 20.17-18 0-31.31-15.06-48.12-15.06-16.14 0-26.66 11-34.78 33.62-27.5 77.55-26.28 74.27-26.12 78.27 0 24.8 97.64 106.11 228.47 106.11M429 254.84c4.65 22 4.65 24.35 4.65 27.25 0 37.66-42.33 58.56-98 58.56-125.74.08-235.91-73.65-235.91-122.33a49.55 49.55 0 0 1 4.06-19.72C58.56 200.86 0 208.93 0 260.63c0 84.67 200.63 189 359.49 189 121.79 0 152.51-55.08 152.51-98.58 0-34.21-29.59-73.05-82.93-96.24"/></svg>
+                            Red Hat
+                        </li>
                         <li>macOS</li>
                     </ul>
                 </li>
