30825b1860be9445f5fc4e77d2478ccf41096b6c
Chano Vera <contacto@chanovera.com>
2023-08-21 19:07:50 -0600

    se crea el archivo resume.php

--- templates/content-resume.php
+++ templates/content-resume.php
@@ -1,6 +1,6 @@
-<main id="main" class="main-content">
+<main id="main">
     <?php include(TEMPLATEPATH . '/parts/sections/title-page.php'); ?>
-    <section class="container">
+    <section class="container main-content">
         <div class="section">
             <h1>Developer</h1>
         </div>
