2a0d5f44915865030ec7af1aef63ae8c2660c80b
Chano Vera <contacto@chanovera.com>
2023-08-22 10:45:53 -0600

    Se agregan ajustes en javascript para content-product.php

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,3 +1,2 @@
-const elementos = document.querySelectorAll('.woocommerce-product-gallery');
-
-elementos.setAttribute("id", "input-qty");
+const inputQty = document.getElementsByClassName("woocommerce-product-gallery__trigger")[0];
+inputQty.setAttribute("id", "input-qty");
