3a956867166f20d36dcbbbb98797cd0e40e4aced
Chano Vera <contacto@chanovera.com>
2023-08-22 10:24:40 -0600

    Se agregan ajustes en javascript para content-product.php

--- assets/js/product.js
+++ assets/js/product.js
@@ -3,11 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Encontrar el elemento con la clase woocommerce-product-gallery__trigger
     var triggerElement = document.querySelector(".woocommerce-product-gallery__trigger");
   
-    // Crear un nuevo contenedor div
-    var container = document.createElement("div");
-    container.classList.add("mi-contenedor"); // Agregar una clase al contenedor si es necesario
+    if (triggerElement) {
+      // Crear un nuevo contenedor div
+      var container = document.createElement("div");
+      container.classList.add("mi-contenedor"); // Agregar una clase al contenedor si es necesario
   
-    // Mover el elemento de disparo dentro del nuevo contenedor
-    triggerElement.parentNode.insertBefore(container, triggerElement);
-    container.appendChild(triggerElement);
+      // Insertar el contenedor antes del elemento de disparo y mover el elemento dentro del contenedor
+      triggerElement.parentNode.insertBefore(container, triggerElement);
+      container.appendChild(triggerElement);
+    }
   });
