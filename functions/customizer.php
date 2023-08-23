edcd57e5ab0e4d2ea8e3b88255e17b1f764232c2
Chano Vera <contacto@chanovera.com>
2023-08-23 16:48:06 -0600

    se cambia el color de linea separatoria para la hoja de contacto desde customizer.php

--- functions/customizer.php
+++ functions/customizer.php
@@ -132,7 +132,7 @@ function theme_colors() {
                 /* sección de contacto */
                 --background-color-contact-section:#c0e4fd;
                 --background-color-input-contact-section:#faefed;
-                --border-color-contact-section:#96d3fc;
+                --border-color-contact-section:#abdcfd;
                 --color-contact-section:#c0606c;
                 
             }
